2057968dbf8623b21772ad26a9d167c5f10c577d
Yu Wu <wuyucsu@gmail.com>
2015-05-30 20:53:33 -0500

    Create Maven project.

---
+++ E_FlashCard/src/main/java/com/mtjwy/E_FlashCard/App.java
@@ -0,0 +1,13 @@
+package com.mtjwy.E_FlashCard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
