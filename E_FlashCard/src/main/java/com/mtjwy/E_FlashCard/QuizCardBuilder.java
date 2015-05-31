6a682c1bd464d74b17bedb0b52e5c4158e1829af
Yu Wu <wuyucsu@gmail.com>
2015-05-30 21:09:47 -0500

    QuizCardBuider outline

---
+++ E_FlashCard/src/main/java/com/mtjwy/E_FlashCard/QuizCardBuilder.java
@@ -0,0 +1,63 @@
+package com.mtjwy.E_FlashCard;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class QuizCardBuilder {
+	
+	public void go() {
+		//build and display gui
+	}
+	
+	private class NextCardListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			//add the current card to the list and clear the text areas
+			
+		}
+		
+	}
+	
+	private class SaveMenuListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			// bring up a file dialog box
+			//let the user name and save the set
+			
+		}
+		
+	}
+	
+	private void saveFile(File file) {
+		//iterate through the list of cards, and write each one out to a text file
+		//in a parseable way
+	}
+	
+	
+	private class NewMenuListener implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+			//clear out the card list, and clear out the text areas
+		}
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
